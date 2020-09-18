37d8975f62bea30ee4eba929f45836c12f30914c
yejihan-dev <yejihanc@gmail.com>
2020-09-19 01:42:54 +0900

    FixMatch: pytorch refactoring

--- ImageDataLoader.py
+++ ImageDataLoader.py
@@ -6,16 +6,46 @@ import torchvision.transforms as transforms
 import numpy as np
 import torch
 
+from randaugment import RandAugmentMC
+
+mean=[0.485, 0.456, 0.406]
+std=[0.229, 0.224, 0.225]
+
 def default_image_loader(path):
     return Image.open(path).convert('RGB')
 
 class TransformTwice:
     def __init__(self, transform):
         self.transform = transform
+
     def __call__(self, inp):
         out1 = self.transform(inp)
         out2 = self.transform(inp)
         return out1, out2    
+
+class TransformFix(object):
+    def __init__(self, mean, std, imResize, imsize):
+        self.weak = transforms.Compose([
+            transforms.Resize(imResize),
+            transforms.RandomResizedCrop(imsize),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std),])
+
+        self.strong = transforms.Compose([
+            transforms.Resize(imResize),
+            transforms.RandomResizedCrop(imsize),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std),
+            RandAugmentMC(n=2, m=10)])
+    
+    def __call__(self, x):
+        weak = self.weak(x)
+        strong = self.strong(x)
+        return weak, strong
     
 class SimpleImageLoader(torch.utils.data.Dataset):
     def __init__(self, rootdir, split, ids=None, transform=None, loader=default_image_loader):
